079879402acc8eb8215cb62e7e0007941e832fb7
dorarad17 <dorarad17@gmail.com>
2020-03-11 08:54:16 +0000

    navbar link set to /home

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,34 +2,34 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Nav() {
-  const navStyle = {
-    color: "orange"
-  };
+	const navStyle = {
+		color: "orange"
+	};
 
-  return (
-    <nav className="navbar p-2">
-      <a class="navbar-brand" href="/" style={navStyle}>
-        Thought Catcher
-      </a>
-      <ul className="nav-links my-auto">
-        <Link style={navStyle} to="/home2">
-          <li>Home</li>
-        </Link>
-        <Link style={navStyle} to="/about">
-          <li>About</li>
-        </Link>
-        <Link style={navStyle} to="/inputs">
-          <li>Inputs</li>
-        </Link>
-        <Link style={navStyle} to="/definitions">
-          <li>Definitions</li>
-        </Link>
-        <Link style={navStyle} to="/">
-          <li>Login</li>
-        </Link>
-      </ul>
-    </nav>
-  );
+	return (
+		<nav className="navbar p-2">
+			<a class="navbar-brand" href="/" style={navStyle}>
+				Thought Catcher
+			</a>
+			<ul className="nav-links my-auto">
+				<Link style={navStyle} to="/home">
+					<li>Home</li>
+				</Link>
+				<Link style={navStyle} to="/about">
+					<li>About</li>
+				</Link>
+				<Link style={navStyle} to="/inputs">
+					<li>Inputs</li>
+				</Link>
+				<Link style={navStyle} to="/definitions">
+					<li>Definitions</li>
+				</Link>
+				<Link style={navStyle} to="/">
+					<li>Login</li>
+				</Link>
+			</ul>
+		</nav>
+	);
 }
 
 export default Nav;
