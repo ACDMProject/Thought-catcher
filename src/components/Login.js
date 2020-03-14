a2e8ef6e9da501460bf0d309b0c380213fe842a0
Alexandra <alexandrajn17@gmail.com>
2020-03-14 12:22:18 +0000

    login updating state

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,49 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Login() {
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <form className="justify-content-center loginForm">
+    <form className="justify-content-center loginForm" onSubmit={handleSubmit}>
       <h2 className="loginHead mb-5">Let's catch some thoughts!</h2>
       <div className="form-group">
-        <label htmlFor="emailHome">Email address</label>
+        <label htmlFor="email">Email address</label>
         <input
           type="email"
           className="form-control"
-          id="emailHome"
-          aria-describedby="emailHelp"
+          id="email"
+          aria-describedby="email"
           placeholder="Enter email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
         />
         <small id="emailHelp" className="form-text text-muted">
           We'll never share your email with anyone else.
         </small>
       </div>
       <div className="form-group">
-        <label htmlFor="exampleInputPassword1">Password</label>
+        <label htmlFor="password">Password</label>
         <input
           type="password"
           className="form-control"
-          id="passwordHome"
+          id="password"
           placeholder="Choose your password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
         />
       </div>
       <div className="form-group form-check">
         <input
           type="checkbox"
           className="form-check-input"
-          id="exampleCheck1"
+          id="rememberMeCheckbox"
         />
-        <label className="form-check-label" htmlFor="exampleCheck1">
+        <label className="form-check-label" htmlFor="rememberMeCheckbox">
           Remember me
         </label>
       </div>
       <button
         type="submit"
         className="btn btnLogin btn-lg btn-block btn-primary"
+        disabled={!validateForm()}
       >
         Login
       </button>
     </form>
   );
 }
-
-export default Login;
