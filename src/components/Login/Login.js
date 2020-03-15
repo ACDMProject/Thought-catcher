38ecd00ad2e55df6cc4a6f4e38b395a14d089cfc
Alexandra <alexandrajn17@gmail.com>
2020-03-15 09:08:35 +0000

    switch between login & register using react hooks

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-// import { Link } from "react-router-dom";
 import Register from "./Register";
 
 export default function Login() {
--- src/components/Login/Register.js
+++ src/components/Login/Register.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import Login from "./Login";
 
 export default function Register() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newUser, setNewUser] = useState(null);
 
   function validateForm() {
     return (
@@ -19,57 +21,83 @@ export default function Register() {
     e.preventDefault();
   }
 
-  return (
-    <form className="justify-content-center loginForm" onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="firstName">First Name</label>
-        <input
-          class="form-control"
-          type="text"
-          placeholder="Enter your first name..."
-          value={firstName}
-          onChange={e => setFirstName(e.target.value)}
-        />
-        <label htmlFor="email">Last Name</label>
-        <input
-          class="form-control"
-          type="text"
-          placeholder="Enter your last name..."
-          value={lastName}
-          onChange={e => setLastName(e.target.value)}
-        />
-        <label htmlFor="email">Email address</label>
-        <input
-          type="email"
-          className="form-control"
-          id="email"
-          aria-describedby="email"
-          placeholder="Enter email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-        <small id="emailHelp" className="form-text text-muted">
-          We'll never share your email with anyone else.
-        </small>
-      </div>
-      <div className="form-group">
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          className="form-control"
-          id="password"
-          placeholder="Choose your password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-      </div>
-      <button
-        type="submit"
-        className="btn btnLogin btn-lg btn-block btn-primary"
-        disabled={!validateForm()}
+  function login() {
+    return (
+      <React.Fragment>
+        <Login />
+      </React.Fragment>
+    );
+  }
+
+  function signUp() {
+    return (
+      <form
+        className="justify-content-center loginForm"
+        onSubmit={handleSubmit}
       >
-        Login
-      </button>
-    </form>
-  );
+        <div className="form-group">
+          <label htmlFor="firstName">First Name</label>
+          <input
+            class="form-control"
+            type="text"
+            placeholder="Enter your first name..."
+            value={firstName}
+            onChange={e => setFirstName(e.target.value)}
+          />
+          <label htmlFor="email">Last Name</label>
+          <input
+            class="form-control"
+            type="text"
+            placeholder="Enter your last name..."
+            value={lastName}
+            onChange={e => setLastName(e.target.value)}
+          />
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            aria-describedby="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Choose your password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="btn btnLogin btn-lg btn-block btn-primary"
+          disabled={!validateForm()}
+        >
+          Login
+        </button>
+        <div>
+          Already registered?{" "}
+          <button
+            type="button"
+            className="btn btn-sm btn-outline-primary"
+            onClick={e => setNewUser(true)}
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    );
+  }
+
+  return <div className="Signup">{newUser === null ? signUp() : login()}</div>;
 }
