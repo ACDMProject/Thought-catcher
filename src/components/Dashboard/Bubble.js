e9c150c3ae8f45a75890529f09b36831c63f0d3e
dorarad17 <dorarad17@gmail.com>
2020-03-15 20:44:14 +0000

    attempt to manipulate data for chart

--- src/components/Dashboard/Bubble.js
+++ src/components/Dashboard/Bubble.js
@@ -6,41 +6,12 @@ import _ from "lodash";
 var moment = require("moment");
 moment().format();
 
-/*
-          // this function will generate output in this format
-          // data = [
-              [timestamp, 23],
-              [timestamp, 33],
-              [timestamp, 12]
-              ...
-          ]
-          */
-
-// data format for buble chart is [
-//     [xaxis,yaxis,size],[xaxis,yaxis,size]
-// ]
-// e.g. we can try to have [date, time of day, mood intensity] with colour representing the type of mood
-// need to update y-axis to be in time format
-
 class Bubble extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			series: [
-				{
-					name: "Anxious",
-					data: [
-						["02-05-2020", "08:11", 5],
-						["02-05-2020", "11:20", 3],
-						["02-07-2020", "13:12", 1],
-						["02-09-2020", "14:10", 4],
-						["02-15-2020", "17:10", 1],
-						["02-16-2020", "20:12", 1],
-						["02-17-2020", "22:12", 5]
-					]
-				}
-			],
+			series: [],
 			options: {
 				chart: {
 					height: 350,
@@ -106,9 +77,39 @@ class Bubble extends Component {
 					thought.eventDate = date;
 					thought.eventTime = time;
 				}
-				//console.log(fullData);
 
-				var filtered = _.map(fullData, function(object) {
+				//create last month in dates
+				var end = moment().format("YYYY-MM-DD");
+				var start = moment()
+					.subtract(1, "months")
+					.format("YYYY-MM-DD");
+				const range = moment.range(start, end);
+
+				console.log(start, end);
+
+				//filter data to be in range of last week
+
+				var lastMonthData = fullData.filter(function(entry) {
+					return range.contains(moment(entry.eventDate)) === true;
+				});
+
+				console.log(lastMonthData);
+
+				const moods = [
+					"Neutral",
+					"Stressed",
+					"Anxious",
+					"Sad",
+					"Angry",
+					"Overwhelmed",
+					"Exhausted",
+					"Happy",
+					"Excited",
+					"Inspired",
+					"Calm"
+				];
+
+				var filtered = _.map(lastMonthData, function(object) {
 					return _.pick(object, [
 						"Mood",
 						"eventDate",
@@ -123,19 +124,37 @@ class Bubble extends Component {
 				});
 
 				console.log(grouped);
-				if (grouped.Mood === "Anxiety") {
-					console.log(grouped);
-				}
+				// if (grouped.Mood === "Anxiety") {
+				// 	console.log(grouped);
+				// }
 
 				// var data = [];
-				// data.push
+				// data.push;
 
-				// var series = {
-				// 	name: "Anxiety",
-				// 	data:
-				// };
+				//need to update series with right data
 
-				// console.log(series);
+				let groups = Object.create(null);
+
+				filtered.forEach((item) => {
+					if (!groups[item.Mood]) {
+						groups[item.Mood] = [];
+					}
+
+					groups[item.Mood].push({
+						x: item.eventDate,
+						y: item.eventTime,
+						z: item.Mood_intensity
+					});
+				});
+
+				let result = Object.entries(groups).map(([k, v]) => ({ [k]: v }));
+
+				console.log(result);
+
+				// this.setState({
+				// 	series: result
+
+				// });
 			})
 
 			.catch(function(error) {
