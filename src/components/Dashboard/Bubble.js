b1877e0118ac3198934914d2c82f675c5bf43f8e
dorarad17 <dorarad17@gmail.com>
2020-03-15 10:09:47 +0000

    data grouped by mood using Lodash

--- src/components/Dashboard/Bubble.js
+++ src/components/Dashboard/Bubble.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import Chart from "react-apexcharts";
 import ReactApexChart from "react-apexcharts";
 import axios from "axios";
+import _ from "lodash";
+var moment = require("moment");
+moment().format();
 
 /*
           // this function will generate output in this format
@@ -12,21 +15,7 @@ import axios from "axios";
               ...
           ]
           */
-function generateData(baseval, count, yrange) {
-	var i = 0;
-	var series = [];
-	while (i < count) {
-		var x = Math.floor(Math.random() * (750 - 1 + 1)) + 1;
-		var y =
-			Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
-		var z = Math.floor(Math.random() * (75 - 15 + 1)) + 15;
 
-		series.push([x, y, z]);
-		baseval += 86400000;
-		i++;
-	}
-	return series;
-}
 // data format for buble chart is [
 //     [xaxis,yaxis,size],[xaxis,yaxis,size]
 // ]
@@ -42,47 +31,13 @@ class Bubble extends Component {
 				{
 					name: "Anxious",
 					data: [
-						["02-04-2020 GMT", 9, 5],
-						["02-03-2020 GMT", 11, 3],
-						["02-07-2020 GMT", 13, 1],
-						["02-09-2020 GMT", 14, 4],
-						["02-15-2020 GMT", 17, 1],
-						["02-16-2020 GMT", 20, 1],
-						["02-17-2020 GMT", 22, 5]
-					]
-				},
-				{
-					name: "Overwhelmed",
-					data: [
-						["02-04-2020 GMT", 9, 5],
-						["02-08-2020 GMT", 12, 3],
-						["02-18-2020 GMT", 15, 1],
-						["02-19-2020 GMT", 16, 4],
-						["02-20-2020 GMT", 18, 2],
-						["02-27-2020 GMT", 22, 1],
-						["02-28-2020 GMT", 23, 5]
-					]
-				},
-				{
-					name: "Excited",
-					data: [
-						["02-04-2020 GMT", 6, 5],
-						["02-05-2020 GMT", 12, 3],
-						["02-09-2020 GMT", 13, 5],
-						["02-21-2020 GMT", 14, 3],
-						["02-27-2020 GMT", 17, 1],
-						["02-28-2020 GMT", 24, 2]
-					]
-				},
-				{
-					name: "Angry",
-					data: [
-						["02-09-2020 GMT", 10, 5],
-						["02-12-2020 GMT", 12, 3],
-						["02-13-2020 GMT", 15, 5],
-						["02-19-2020 GMT", 15, 3],
-						["02-11-2020 GMT", 21, 1],
-						["02-02-2020 GMT", 24, 2]
+						["02-05-2020", 9, 5],
+						["02-05-2020", 11, 3],
+						["02-07-2020", 13, 1],
+						["02-09-2020", 14, 4],
+						["02-15-2020", 17, 1],
+						["02-16-2020", 20, 1],
+						["02-17-2020", 22, 5]
 					]
 				}
 			],
@@ -132,6 +87,71 @@ class Bubble extends Component {
 			}
 		};
 	}
+
+	// component life cycle method
+	componentDidMount = () => {
+		axios
+			.get(
+				"https://2xi4uzqzba.execute-api.eu-west-2.amazonaws.com/dev/Thoughts"
+			)
+			.then((response) => {
+				// handle success
+
+				var data = [];
+
+				let fullData = response.data.thoughts;
+				//console.log(fullData);
+				for (let i = 0; i < fullData.length; i++) {
+					let thought = fullData[i];
+					let date = moment.utc(thought.Timestamp).format("YYYY-MM-DD");
+					let time = moment.utc(thought.Timestamp).format("HH:MM");
+					thought.eventDate = date;
+					thought.eventTime = time;
+				}
+				//console.log(fullData);
+
+				var filtered = _.map(fullData, function(object) {
+					return _.pick(object, [
+						"Mood",
+						"eventDate",
+						"eventTime",
+						"Mood_intensity"
+					]);
+				});
+				console.log(filtered);
+
+				var grouped = _.groupBy(filtered, function(thought) {
+					return thought.Mood;
+				});
+
+				console.log(grouped);
+
+				var series = {};
+
+				console.log(series);
+			})
+
+			// function generateData(baseval, count, yrange) {
+			// 	var i = 0;
+			// 	var series = [];
+			// 	while (i < count) {
+			// 		var x = Math.floor(Math.random() * (750 - 1 + 1)) + 1;
+			// 		var y =
+			// 			Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+			// 		var z = Math.floor(Math.random() * (75 - 15 + 1)) + 15;
+
+			// 		series.push([x, y, z]);
+			// 		baseval += 86400000;
+			// 		i++;
+			// 	}
+			// 	return series;
+			// }
+
+			.catch(function(error) {
+				// handle error
+				console.error(error);
+			});
+	};
 
 	render() {
 		return (
