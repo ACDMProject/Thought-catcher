fcd03fea467ceec43ce8645563cdd5eaf2756b9d
Charlotte House <cdmhouse@sky.com>
2020-03-20 23:25:26 +0000

    header brought back in as erroring

--- src/components/FirstPage/Header.js
+++ src/components/FirstPage/Header.js
@@ -1,4 +1,34 @@
 import React from "react";
 
 
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <header id="header">
+          <div className="content">
+           
+            <h6 className="slogan">
+              a place to write and understand your thoughs...
+            </h6>
+            <div className="container">
+              <ul className="actions">
+                <div className="btn-group">
+                  <button type="button" className="btn-sign-in">
+                    <div>Log in</div>
+                  </button>
+                  <button type="button" className="btn-sign-up">
+                    <span>Sign up</span>
+                  </button>
+                </div>
+              </ul>
+            </div>
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
 export default Header;
