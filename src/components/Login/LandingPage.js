6f6a5eafdc8a883c8dea551c5afb9fb9388d5120
dorarad17 <dorarad17@gmail.com>
2020-04-18 17:00:58 +0100

    edit image path

--- src/components/Login/LandingPage.js
+++ src/components/Login/LandingPage.js
@@ -2,23 +2,23 @@ import React from "react";
 import Login from "./Login";
 
 function LandingPage() {
-  return (
-    <div className="container-fluid">
-      <div className="row align-items-center">
-        <div className="col-lg-7">
-          <img
-            src="/images/tspCatchThoughtCrop.png"
-            alt="catchThought"
-            id="catchThought"
-            className="img-fluid rounded mx-auto d-block"
-          />
-        </div>
-        <div className="col-lg-4">
-          <Login />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="container-fluid">
+			<div className="row align-items-center">
+				<div className="col-lg-7">
+					<img
+						src="../images/tspCatchThoughtCrop.png"
+						alt="catchThought"
+						id="catchThought"
+						className="img-fluid rounded mx-auto d-block"
+					/>
+				</div>
+				<div className="col-lg-4">
+					<Login />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default LandingPage;
