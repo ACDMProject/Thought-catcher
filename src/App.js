13c799432b930fb63650c0312ca9a84757cdec11
dorarad17 <dorarad17@gmail.com>
2020-04-18 16:44:15 +0100

    added hashrouter

--- src/App.js
+++ src/App.js
@@ -10,32 +10,37 @@ import Login from "./components/Login/LandingPage";
 import Register from "./components/Login/Register";
 import Nav from "./components/Nav";
 import Dashboard from "./components/Dashboard/Dashboard";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	HashRouter,
+} from "react-router-dom";
 
 function App() {
-  // const Home = () => (
-  // 	<div>
-  // 		<Home />
-  // 	</div>
-  // );
+	// const Home = () => (
+	// 	<div>
+	// 		<Home />
+	// 	</div>
+	// );
 
-  return (
-    <Router>
-      <div className="App">
-        <Nav />
-        <Switch>
-          <Route path="/" exact component={LandingPage} />
-          <Route path="/login" exact component={Login} />
-          <Route path="/register" exact component={Register} />
-          <Route path="/home" exact component={Home} />
-          <Route path="/moodscriber" component={MoodScriber} />
-          <Route path="/diary" component={Diary} />
-          <Route path="/definitions" component={Definitions} />
-          <Route path="/dashboard" component={Dashboard} />
-        </Switch>
-      </div>
-    </Router>
-  );
+	return (
+		<HashRouter basename="/">
+			<div className="App">
+				<Nav />
+				<Switch>
+					<Route path="/" exact component={LandingPage} />
+					<Route path="/login" exact component={Login} />
+					<Route path="/register" exact component={Register} />
+					<Route path="/home" exact component={Home} />
+					<Route path="/moodscriber" component={MoodScriber} />
+					<Route path="/diary" component={Diary} />
+					<Route path="/definitions" component={Definitions} />
+					<Route path="/dashboard" component={Dashboard} />
+				</Switch>
+			</div>
+		</HashRouter>
+	);
 }
 
 export default App;
