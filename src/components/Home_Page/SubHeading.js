6515a80d0919338712d8211d25a9ccc6fcf2cac8
dorarad17 <dorarad17@gmail.com>
2020-04-18 17:37:03 +0100

    moved images and changed to relative paths to fix bugs in gh-pages deploy

--- src/components/Home_Page/SubHeading.js
+++ src/components/Home_Page/SubHeading.js
@@ -1,29 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import homeImage from "./3364392.jpg";
 
 function SubHeading() {
-  const navStyle = {
-    color: "purple"
-  };
-  return (
-    <div>
-      <p className="sub-heading shadow-sm mb-0">
-        CBT focuses on how your thoughts affect your moods and teaches you
-        skills for dealing with negative thinking. A good way to kick negative
-        thinking is to catch your thoughts and identify which thinking patterns
-        have been getting you down.
-        <Link style={navStyle} to="/moodscriber">
-          {" "}
-          Ready to log some thoughts?
-        </Link>
-      </p>
-      <img
-        src="/images/3364392.jpg"
-        alt="home-pic"
-        id="home-pic"
-        className="img-fluid pic-radius shadow-sm"
-      />
-    </div>
-  );
+	const navStyle = {
+		color: "purple",
+	};
+	return (
+		<div>
+			<p className="sub-heading shadow-sm mb-0">
+				CBT focuses on how your thoughts affect your moods and teaches you
+				skills for dealing with negative thinking. A good way to kick negative
+				thinking is to catch your thoughts and identify which thinking patterns
+				have been getting you down.
+				<Link style={navStyle} to="/moodscriber">
+					{" "}
+					Ready to log some thoughts?
+				</Link>
+			</p>
+			<img
+				src={homeImage}
+				alt="home-pic"
+				id="home-pic"
+				className="img-fluid pic-radius shadow-sm"
+			/>
+		</div>
+	);
 }
 export default SubHeading;
--- src/components/MoodScriber.js
+++ src/components/MoodScriber.js
@@ -1,26 +1,27 @@
 import React from "react";
 import MasterForm from "./MultiStepForm/MasterForm";
+import scriberImage from "./3366283.png";
 
 function MoodScriber() {
-  return (
-    <div className="container-fluid moodscriber">
-      <div className="row d-flex justify-content-around align-items-center">
-        <div className="col-lg-5 ">
-          <h4 className="mt-1 mb-4">Mood Scriber </h4>
-          <MasterForm />
-        </div>
+	return (
+		<div className="container-fluid moodscriber">
+			<div className="row d-flex justify-content-around align-items-center">
+				<div className="col-lg-5 ">
+					<h4 className="mt-1 mb-4">Mood Scriber </h4>
+					<MasterForm />
+				</div>
 
-        <div className="col-lg-5 mt-5">
-          <img
-            src="/images/3366283.png"
-            alt="catchThought"
-            id="catchThought"
-            className="img-fluid"
-          />
-        </div>
-      </div>
-    </div>
-  );
+				<div className="col-lg-5 mt-5">
+					<img
+						src={scriberImage}
+						alt="catchThought"
+						id="catchThought"
+						className="img-fluid"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default MoodScriber;
