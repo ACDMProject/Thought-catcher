42e95ed3dff177df3c7b5f7d53b70dd837356023
dorarad17 <dorarad17@gmail.com>
2020-04-25 15:19:52 +0100

    Updates

---
+++ precache-manifest.6170c4c6742c0a7f8072c281f4e0640e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22dc552084f646ea6384768077499f33",
+    "url": "/Thought-catcher/index.html"
+  },
+  {
+    "revision": "593d0fdd685992bd2624",
+    "url": "/Thought-catcher/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "d9a3997e18d80b005e98",
+    "url": "/Thought-catcher/static/css/main.3fb8c5ce.chunk.css"
+  },
+  {
+    "revision": "593d0fdd685992bd2624",
+    "url": "/Thought-catcher/static/js/2.81581886.chunk.js"
+  },
+  {
+    "revision": "ccbb9836384252930e3bbeb9a02b5166",
+    "url": "/Thought-catcher/static/js/2.81581886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a3997e18d80b005e98",
+    "url": "/Thought-catcher/static/js/main.5fa8bfaa.chunk.js"
+  },
+  {
+    "revision": "70c41e3f7023d1c2efca",
+    "url": "/Thought-catcher/static/js/runtime-main.cf3186a0.js"
+  },
+  {
+    "revision": "8a804db96138e63d33862b4640c376e3",
+    "url": "/Thought-catcher/static/media/3364392.8a804db9.jpg"
+  },
+  {
+    "revision": "0bd20bcbbda2c3802af0eacb97dcc65e",
+    "url": "/Thought-catcher/static/media/3366283.0bd20bcb.png"
+  },
+  {
+    "revision": "5f263296ae0b45a5f34fdd8679870240",
+    "url": "/Thought-catcher/static/media/BlackWhite.5f263296.png"
+  },
+  {
+    "revision": "66cf685529ff1dea9a3b6006ba0a47a1",
+    "url": "/Thought-catcher/static/media/Catastrophisation.66cf6855.png"
+  },
+  {
+    "revision": "4056f055052d175d2b796eca3623f9ca",
+    "url": "/Thought-catcher/static/media/Dim.4056f055.png"
+  },
+  {
+    "revision": "9a4a0d22885e83590085e2cb086ac41e",
+    "url": "/Thought-catcher/static/media/EmotionalReasoning.9a4a0d22.png"
+  },
+  {
+    "revision": "532142aff942cc3873e15ea7cf2f9e37",
+    "url": "/Thought-catcher/static/media/JumpingToConclusions.532142af.png"
+  },
+  {
+    "revision": "d92f707ac888fc263e7b7606160970df",
+    "url": "/Thought-catcher/static/media/Logo.d92f707a.png"
+  },
+  {
+    "revision": "69478fb2f00125f1e3c15c592ef10514",
+    "url": "/Thought-catcher/static/media/MentalFilter.69478fb2.png"
+  },
+  {
+    "revision": "3c73e888cc35d0bbd32cd9804e2a3cc2",
+    "url": "/Thought-catcher/static/media/Overgeneralisation.3c73e888.png"
+  },
+  {
+    "revision": "f473c6db742bb6467e9d6a20f2808551",
+    "url": "/Thought-catcher/static/media/Personalisation.f473c6db.png"
+  },
+  {
+    "revision": "88ac802f4139213bf4591115f8992071",
+    "url": "/Thought-catcher/static/media/Shoulds.88ac802f.png"
+  },
+  {
+    "revision": "d92be74cba28dc9de3801a40d8d19c99",
+    "url": "/Thought-catcher/static/media/UniversalLabelling.d92be74c.png"
+  },
+  {
+    "revision": "cf0d374aa53f714abfe2abac1daa97fc",
+    "url": "/Thought-catcher/static/media/tspCatchThoughtCrop.cf0d374a.png"
+  }
+]);
